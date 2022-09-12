1f63598a4d63b67bd10160a4f6639cefd68359c3
DimzGanz <93122220+DimzGanz@users.noreply.github.com>
2022-09-12 17:37:16 +0700

    Add files via upload

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,12 +2,7 @@ let fetch = require('node-fetch')
 let teks = 'gcbot'
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
 
-*𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐉𝐚𝐧𝐠𝐚𝐧 𝐋𝐮𝐩𝐚 𝐉𝐨𝐢𝐧 && 𝐁𝐚𝐜𝐚 𝐑𝐮𝐥𝐞𝐬❗*
-╔════════════════════
-║ *Group Random*
-╠════════════════════
-║https://tinyurl.com/2h6gch82
-╚════════════════════
+*Gwehj Gapunya Group* 
 
 `.trim(), wm, 'Owner', '.owner', 'Menu', '.menu')
 handler.help = ['gcbot']
