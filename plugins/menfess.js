96dd0532d78505ff2a0b2ed59a1e15de02e473c2
DimzGanz <93122220+DimzGanz@users.noreply.github.com>
2022-09-15 16:14:46 +0700

    Add files via upload

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -4,12 +4,12 @@ let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
 let [number, pesan] = text.split `|`
 
-    if (!number) return conn.reply(m.chat, 'Penggunaan .menfess nomor|pesan untuknya\nContoh : .menfess 628xxxxxxxxxx|hai owner', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
-    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
-    if (text > 700) return conn.reply(m.chat, 'Teks Kepanjangan!', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
+    if (!number) return conn.reply(m.chat, 'Penggunaan .menfess nomor|pesan untuknya\nContoh : .menfess 628xxxxxxxxxx|hai owner', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),
+        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),}}})
+    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),
+        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),}}})
+    if (text > 700) return conn.reply(m.chat, 'Teks Kepanjangan!', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),
+        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6289696764138?text=.chat 6289696764138|Hi Dimas Hehehe 🌹', body: 'DimzBot', thumbnail: await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(),}}})
 
     let chat = `${number}`
     var nomor = m.sender
@@ -33,7 +33,7 @@ Contoh Penggunaan: .menfess nomor|pesan untuknya
 
 Contoh: #menfess 628xxxxxxxxxx|hai owner`
 
-    conn.send2ButtonImg(chat + '@s.whatsapp.net', await(await fetch('https://telegra.ph/file/aa06b2e4067f6a6387b75.jpg')).buffer(), chat1, jarot, 'Menu', '.menu', 'Donasi', '.donasi', m)
+    conn.send2ButtonImg(chat + '@s.whatsapp.net', await(await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(), chat1, jarot, 'Menu', '.menu', 'Donasi', '.donasi', m)
     
     let logs = `NOTE: DILARANG SPAM, CHAT ANEH ANEH, BERANTEM. JIKA ITU TERJADI MAKA BOT AKAN MEMBLOKIR KONTAKMU!!`
 
@@ -45,7 +45,7 @@ Contoh: #menfess 628xxxxxxxxxx|hai owner`
 Isi Pesan : ${pesan}
 
 ⬡──⬡─────────⬡──⬡`
-    conn.send2ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/9ceda8d2274e97bad5bca.jpg')).buffer(), logs, haori1, 'Owner', '.owner', 'Donasi', '.donasi', m)
+    conn.send2ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/4189454f16526ee66e68d.jpg')).buffer(), logs, haori1, 'Owner', '.owner', 'Donasi', '.donasi', m)
 }
 handler.help = ['menfess <nomor|pesan>']
 handler.tags = ['main']
